4d3fab34dd4d8a2dd96d18bf827f4e7f78151425
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-09-07 20:54:37 +0400

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PredictView, UserFeedbackView
+from .views import PredictView, UserFeedbackView, RetrainingView
 
 
 urlpatterns = [
     path('predict/', PredictView.as_view(), name='prediction'),
     path('feedback/<int:id_predict>/', UserFeedbackView.as_view(), name='feedback'),
+    path('train-model/', RetrainingView.as_view(), name='train-model'),
 ]
